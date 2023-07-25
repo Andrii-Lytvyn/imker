6fd28c745d241f0e3a3b928ad4b55310fbdf8958
Valentyna <oreshkav@gmail.com>
2023-07-25 23:15:29 +0200

    delete css

--- src/сomponents/AboutUs/AboutUs.tsx
+++ src/сomponents/AboutUs/AboutUs.tsx
@@ -1,30 +1,31 @@
-import styles from "./AboutUs.module.css";
-
 export default function AboutUs(): JSX.Element {
+
+
+
 
     return (
         <>
-            <div className={styles.title_container}>
-                <div className={styles.title}>
+            <div>
+            <div>
                     <span>ABOUT US</span>
                 </div>
             </div>
 
-            <div className={styles.description_container}>
-                <div className={styles.description_right_and_left}>
-                    <div className={styles.description_left}>
-                        <div className={styles.left_top}>
+            <div>
+            <div>
+                <div>
+                    <div>
                             <p> Left block. WHAT ARE HONEYBEES </p>
                         </div>
-                        <div className={styles.left_img}>
-                            <img className={styles.description_left_img} src="../public/img/bee.jpg"/>
+                        <div>
+                            <img  src=""/>
                         </div>
-                        <div className={styles.left_bottom}>
+                        <div>
                             <p> Left block. Bottom. Text. </p>
                         </div>
                     </div>
-                    <div className={styles.description_right}>
-                        <div className={styles.description_right_top}>
+                    <div>
+                    <div>
                             <p> Right block. Text top
                                 <br />
                                 Lorem ipsum dolor sit amet, ad nec scripta volumus,
@@ -33,15 +34,15 @@ export default function AboutUs(): JSX.Element {
                                 ne legere quodsi phaedrum ad per, in malis.
                             </p>
                         </div>
-                        <div className={styles.description_right_middle_img}>
+                        <div>
                             <div>
-                                <img className={styles.description_img} src="../public/img/bee.jpg" />
+                                <img src="" />
                             </div>
                             <div>
-                                <img className={styles.description_img} src="../public/img/bee.jpg"  />
+                                <img src=""  />
                             </div>
                         </div>
-                        <div className={styles.description_right_bottom}>
+                        <div>
                             <p> Right block. Text bottom
                                 <br /><br />
                                 Ponderum consulatu cum te Lorem ipsum Vel ad falli
@@ -52,20 +53,20 @@ export default function AboutUs(): JSX.Element {
                     </div>
                 </div>
             </div>
-            <div className={styles.team_container}>
-                    <div className={styles.team_title}>
-                        <div className={styles.title}>
+            <div>
+            <div>
+                    <div>
                             <h2>OUR TEAM</h2><br/>
                         </div>
                         <div>
                             <p>Text .... Meet our team and so on</p>
                         </div>
                     </div>
-                    <div className={styles.members}>
+                    <div>
 
-                        <div className={styles.member1}>
+                        <div>
                             <div>
-                            <img className={styles.memeber_img} src="../public/img/beeboss.jpg" />
+                            <img src="../public/img/beeboss.jpg" />
                             </div>
                             <div>
                             <p> Members 1 Position</p>
@@ -76,21 +77,21 @@ export default function AboutUs(): JSX.Element {
                             <div>
                             <p> Members 1 Description</p>
                             </div>
-                            <div className={styles.members_networks}>
+                            <div>
                             <p> Members 1 Facebook</p>
                             <p> Members 1 Instagramm</p>
                             <p> Members 1 Email</p>                         
                             </div>
                         </div>
 
-                        <div className={styles.member}>
-                            <img className={styles.memeber_img} src="../public/img/beeboss.jpg" />
+                        {/* <div>
+                            <img src="../public/img/beeboss.jpg" />
                             <p> Members 2 </p>
                         </div>
-                        <div className={styles.member}>
-                            <img className={styles.memeber_img} src="../public/img/beeboss.jpg" />
+                        <div >
+                            <img src="../public/img/beeboss.jpg" />
                             <p> Members 3 </p>
-                        </div>
+                        </div> */}
                     </div>
             </div>
         </>

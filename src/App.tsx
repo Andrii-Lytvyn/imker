b061cc0008f9b29070dbd1f1bbdc1db65dad9153
Golik Andrii <Golik.Andrii@gmail.com>
2023-07-25 21:46:11 +0200

    Add components AboutUs

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import MainPage from "./сomponents/MainPage/MainPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Contacts from "./сomponents/Сontacts/Contacts";
 import Login from "./сomponents/UserLogin/Login";
+import AboutUs from "./сomponents/AboutUs/AboutUs";
 
 function App(): JSX.Element {
   return (
@@ -25,6 +26,12 @@ function App(): JSX.Element {
                 <Route
                     path='/imker/contact'
                     element={<Contacts />}
+                    exact
+                >
+                </Route>
+                <Route
+                    path='/imker/about'
+                    element={<AboutUs />}
                     exact
                 >
                 </Route>

084e724e2b4a7e0390aa0e61fac97ddab6ad20ae
Golik Andrii <Golik.Andrii@gmail.com>
2023-08-02 17:33:31 +0200

    Fix Header (DE) and fix routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,20 @@ import MainPage from "./сomponents/MainPage/MainPage";
 import Login from "./сomponents/UserLogin/Login";
 import AboutUs from "./сomponents/AboutUs/AboutUs";
 import NoPage from "./сomponents/NoPage/NoPage";
+import Contacts from "./сomponents/ContactUs/ContactUs";
 
 function App(): JSX.Element {
   return (
     <>
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/posts" element={<NoPage />} />
+        <Route path="/posts/{post-id}" element={<MainPage />} />
+        <Route path="/events" element={<NoPage />} />
+        <Route path="/events/{event-id}" element={<MainPage />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/contacts" element={<Contacts />} />
-        <Route path="/about" element={<AboutUs />} />
+        <Route path="/contactUs" element={<Contacts />} />
+        <Route path="/aboutUs" element={<AboutUs />} />
         <Route path="/*" element={<NoPage />} />
       </Routes>
       <ToastContainer
--- src/сomponents/Header/Header.tsx
+++ src/сomponents/Header/Header.tsx
@@ -2,8 +2,8 @@ import styles from "./Header.module.css";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-// import { BsPersonCircle } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import {NavDropdown} from "react-bootstrap";
 
 export default function Header(): JSX.Element {
   return (
@@ -19,26 +19,33 @@ export default function Header(): JSX.Element {
               <span>LOGO</span>
             </Link>
           </Nav>
-          <Nav className="d-flex justify-content-evenly">
-            <Link to="/">
-              <img src="/img/flags/en_50.png" width="50%" />
-            </Link>
-            <Link to="/">
-              <img src="/img/flags/de_50.png" width="50%" />
-            </Link>
-          </Nav>
           <Nav
             className={
               styles.linkMenu + " justify-content-between align-items-center"
             }
           >
-            <Link to="/">Pages</Link>
-            <Link to="/">Products</Link>
-            <Link to="/">Blog</Link>
-            <Link to="/">Gallery</Link>
-            <Link to="/contactus">Contact Us</Link>
-            <Link to="/contactusadm">Contact Us Adm</Link>
-            <Link to="/404">Page 404</Link>
+            <Link to="/posts">Blog</Link>
+            <Link to="/events">Veranstaltungen</Link>
+            <NavDropdown title="Über uns" id="basic-nav-dropdown">
+              <NavDropdown.Item>
+                <Link to="/contactUs">Kontakt</Link>
+              </NavDropdown.Item>
+              <NavDropdown.Item>
+                <Link to="/aboutUs">Mitglieder der Gemeinschaft</Link>
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Link to="/gallery">Galerie</Link>
+            <NavDropdown title="Für test" id="basic-nav-dropdown">
+              <NavDropdown.Item>
+
+                {/*
+                Сюда вставляем ссылки для тестирования компонентов. Не забываем поменять в App.tsx
+                После тестирования, пожалуйста, очистите за собой App.tsx
+                */}
+                <Link to="/contacts">Test link</Link>
+
+              </NavDropdown.Item>
+            </NavDropdown>
             <Link to="/login">
               <span>LogIn</span>
             </Link>

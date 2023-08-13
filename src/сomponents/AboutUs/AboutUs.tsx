f7efa26fe5c8c289334beeb2d5c123bc1be8851f
Valentyna <oreshkav@gmail.com>
2023-08-13 16:50:46 +0200

    Connected in AboutUs Front and Back

--- src/сomponents/AboutUs/AboutUs.tsx
+++ src/сomponents/AboutUs/AboutUs.tsx
@@ -1,101 +1,113 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { IMember } from "./IMembers";
+
+
 export default function AboutUs(): JSX.Element {
-  return (
-    <>
-      <div>
-        <div>
-          <span>ABOUT US</span>
-        </div>
-      </div>
 
-      <div>
-        <div>
-          <div>
-            <div>
-              <p> Left block. WHAT ARE HONEYBEES </p>
-            </div>
-            <div>
-              <img src="" />
-            </div>
-            <div>
-              <p> Left block. Bottom. Text. </p>
-            </div>
-          </div>
-          <div>
-            <div>
-              <p>
-                {" "}
-                Right block. Text top
-                <br />
-                Lorem ipsum dolor sit amet, ad nec scripta volumus, eu viris
-                salutatus dissentias sit, ex enim Duo magna nostro persequeris
-                ne Eam tritani maiorum ne, quod ne legere quodsi phaedrum ad
-                per, in malis.
-              </p>
-            </div>
-            <div>
-              <div>
-                <img src="" />
-              </div>
-              <div>
-                <img src="" />
-              </div>
-            </div>
-            <div>
-              <p>
-                {" "}
-                Right block. Text bottom
-                <br />
-                <br />
-                Ponderum consulatu cum te Lorem ipsum Vel ad falli graecis
-                copiosae, solum integre fastidii sea cu. Melius insolens
-                constituto ad pri, numquam accommodare eu nec. Pro ad wisi
-                altera forensibus. Et pri nemore nomina{" "}
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div>
-        <div>
-          <div>
-            <h2>OUR TEAM</h2>
-            <br />
-          </div>
-          <div>
-            <p>Text .... Meet our team and so on</p>
-          </div>
-        </div>
-        <div>
-          <div>
-            <div>
-              <img src="../public/img/beeboss.jpg" />
-            </div>
-            <div>
-              <p> Members 1 Position</p>
-            </div>
-            <div>
-              <p> Members 1 Name</p>
-            </div>
-            <div>
-              <p> Members 1 Description</p>
-            </div>
-            <div>
-              <p> Members 1 Facebook</p>
-              <p> Members 1 Instagramm</p>
-              <p> Members 1 Email</p>
-            </div>
-          </div>
+    const [member, setMember] = useState<IMember[]>([]);
+    console.log("🚀 ~ file: AboutUs.tsx:9 ~ AboutUs ~ member:", member)
 
-          {/* <div>
-                            <img src="../public/img/beeboss.jpg" />
-                            <p> Members 2 </p>
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get("http://localhost:8080/api/members");
+                const memberDto = await response.data;
+                setMember(memberDto.members);
+                console.log("🚀 ~ file: AboutUs.tsx:28 ~ fetchData ~ memberDto:", memberDto)
+            } catch (error) {
+                console.error("Error during request execution:", error);
+            }
+        };
+        fetchData();
+    }, []);
+
+    return (
+        <>
+            <div>
+                <div>
+                    <span>ABOUT US</span>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <div>
+                        <p> Left block top. WHAT ARE HONEYBEES </p>
+                    </div>
+                    <div>
+                        <img src="" />
+                    </div>
+                    <div>
+                        <p> Left block. Bottom. Text. </p>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <p> Right block. Text top
+                            <br />
+                            Lorem ipsum dolor sit amet, ad nec scripta volumus,
+                            eu viris salutatus dissentias sit, ex enim Duo magna
+                            nostro persequeris ne Eam tritani maiorum ne, quod
+                            ne legere quodsi phaedrum ad per, in malis.
+                        </p>
+                    </div>
+                    <div>
+                        <div>
+                            <img src="" />
                         </div>
-                        <div >
-                            <img src="../public/img/beeboss.jpg" />
-                            <p> Members 3 </p>
-                        </div> */}
-        </div>
-      </div>
-    </>
-  );
+                        <div>
+                            <img src="" />
+                        </div>
+                    </div>
+                    <div>
+                        <p> Right block. Text bottom
+                            <br /><br />
+                            Ponderum consulatu cum te Lorem ipsum Vel ad falli
+                            graecis copiosae, solum integre fastidii sea cu. Melius
+                            insolens constituto ad pri, numquam accommodare eu nec.
+                            Pro ad wisi altera forensibus. Et pri nemore nomina </p>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <div>
+                        <h2>OUR TEAM</h2><br />
+                    </div>
+                    <div>
+                        <p>Text .... Meet our team and so on</p>
+                    </div>
+                </div>
+                <div>
+                    <ul>
+                        {member?.map((element, index) => (
+                            element.state === "SHOW" ?
+                                <li key={index}>
+                                    <div>
+                                        <div>
+                                            <img src={element.image} />
+                                        </div>
+                                        <div>
+                                            <p>{element.position}</p>
+                                        </div>
+                                        <div>
+                                            <p> {element.name}</p>
+                                        </div>
+                                        <div>
+                                            <p>{element.description}</p>
+                                        </div>
+                                        <div>
+                                            <a href={element.facebook}> Facebook </a>
+                                            <a href={element.instagram}> Instagramm </a>
+                                            <a href={element.email}> Email </a>
+                                        </div>
+                                    </div>
+                                </li>
+                                : ""
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </>
+    );
 }

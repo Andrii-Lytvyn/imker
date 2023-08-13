00cb00a4769f6d9f5f3d1736bdf5a644385195b4
Golik Andrii <Golik.Andrii@gmail.com>
2023-08-13 19:41:49 +0200

    Add style for About Us component

--- src/сomponents/AboutUs/AboutUs.tsx
+++ src/сomponents/AboutUs/AboutUs.tsx
@@ -1,101 +1,73 @@
+import styles from "./AboutUs.module.css";
+import {Container} from "react-bootstrap";
+import {FaFacebook, FaInstagram, FaTwitter} from "react-icons/fa6";
 export default function AboutUs(): JSX.Element {
   return (
     <>
-      <div>
-        <div>
-          <span>ABOUT US</span>
-        </div>
-      </div>
 
-      <div>
-        <div>
+      <Container>
+        <h1>Über uns</h1>
+      </Container>
+      <Container className="d-flex flex-column">
+                     <h4>WAS SIND HONIGBIENEN?</h4>
+                <div className="mb-3">
+                    <p className={styles.about_text}>Lorem ipsum dolor sit amet, ad nec scripta volumus, eu viris salutatus dissentias sit, ex enim Duo magna nostro persequeris ne Eam tritani maiorum ne, quod ne legere quodsi phaedrum ad per, in malis.</p>
+                    <p className={styles.about_text}>Vel ad falli graecis copiosae, solum integre fastidii sea cu. Melius insolens constituto ad pri, numquam accommodare eu nec. Pro ad wisi altera forensibus.</p>
+                </div>
+                <div className="d-flex justify-content-around">
+                      <div className={styles.about_img +" d-flex flex-column justify-around p-3"}>
+                          <img src="img/about_1.jpg" width="90%" />
+                          <caption>"Bright as a sunflower."</caption>
+                      </div>
+                        <div className={styles.about_img +" d-flex flex-column justify-around p-3"}>
+                        <img src="img/about_2.jpg" width="90%" />
+                        <caption>"Bright as a sunflower."</caption>
+                    </div>
+                </div>
           <div>
-            <div>
-              <p> Left block. WHAT ARE HONEYBEES </p>
-            </div>
-            <div>
-              <img src="" />
-            </div>
-            <div>
-              <p> Left block. Bottom. Text. </p>
-            </div>
+              <p className={styles.about_text + " mb-3"}>Ponderum consulatu cum te Lorem ipsum Vel ad falli graecis copiosae, solum integre fastidii sea cu. Melius insolens constituto ad pri, numquam accommodare eu nec. Pro ad wisi altera forensibus. Et pri nemore nominati. Sit an vidisse propriae apeirian, nec persecuti appellantur te. Vel ei prima Mea mentitum instructior ne, argumentum suscipiantur ut nam Simul euis Laboramus neglegentur at ius. Ut sed assum inte.</p>
+              <p className={styles.about_text + " mb-3"}>Ponderum consulatu cum te Lorem ipsum Vel ad falli graecis copiosae, solum integre fastidii sea cu. Melius insolens constituto ad pri, numquam accommodare eu nec. Pro ad wisi altera forensibus. Et pri nemore nominati. Sit an vidisse propriae apeirian, nec persecuti appellantur te. Vel ei prima Mea mentitum instructior ne, argumentum suscipiantur ut nam Simul euis Laboramus neglegentur at ius. Ut sed assum inte.</p>
           </div>
-          <div>
-            <div>
-              <p>
-                {" "}
-                Right block. Text top
-                <br />
-                Lorem ipsum dolor sit amet, ad nec scripta volumus, eu viris
-                salutatus dissentias sit, ex enim Duo magna nostro persequeris
-                ne Eam tritani maiorum ne, quod ne legere quodsi phaedrum ad
-                per, in malis.
-              </p>
-            </div>
-            <div>
-              <div>
-                <img src="" />
-              </div>
-              <div>
-                <img src="" />
-              </div>
-            </div>
-            <div>
-              <p>
-                {" "}
-                Right block. Text bottom
-                <br />
-                <br />
-                Ponderum consulatu cum te Lorem ipsum Vel ad falli graecis
-                copiosae, solum integre fastidii sea cu. Melius insolens
-                constituto ad pri, numquam accommodare eu nec. Pro ad wisi
-                altera forensibus. Et pri nemore nomina{" "}
-              </p>
-            </div>
-          </div>
-        </div>
+      </Container>
+      <div className={styles.our_team}>
+          <h2 className="mt-3">UNSER EXPERTENTEAM</h2>
+          <h4>Lernen Sie unser leidenschaftliches Team von Honigproduktionsprofis kennen</h4>
       </div>
-      <div>
-        <div>
-          <div>
-            <h2>OUR TEAM</h2>
-            <br />
+      <Container className="d-flex">
+          <div className={styles.about_members}>
+                <img src="img/team1.jpg"/>
+              <p className={styles.about_position}>BEEKEEPER</p>
+              <p className={styles.about_position_name}>GREGORY SILVA</p>
+              <p className={styles.about_position_text}>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatupteoccaecat cupidatat not perspiciatis unde omnise natus error sit voluptatem accus.</p>
+              <div className={styles.social}>
+                  <a href="#"><FaFacebook className={styles.social_icons} /></a>
+                  <a href="#"><FaInstagram className={styles.social_icons} /></a>
+                  <a href="#"><FaTwitter className={styles.social_icons} /></a>
+              </div>
           </div>
-          <div>
-            <p>Text .... Meet our team and so on</p>
+          <div className={styles.about_members}>
+              <img src="img/team2.jpg"/>
+              <p className={styles.about_position}>FIELD TECHNICIAN</p>
+              <p className={styles.about_position_name}>ROGER PORTER</p>
+              <p className={styles.about_position_text}>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatupteoccaecat cupidatat not perspiciatis unde omnise natus error sit voluptatem accus.</p>
+              <div className={styles.social}>
+                  <a href="#"><FaFacebook className={styles.social_icons} /></a>
+                  <a href="#"><FaInstagram className={styles.social_icons} /></a>
+                  <a href="#"><FaTwitter className={styles.social_icons} /></a>
+              </div>
           </div>
-        </div>
-        <div>
-          <div>
-            <div>
-              <img src="../public/img/beeboss.jpg" />
-            </div>
-            <div>
-              <p> Members 1 Position</p>
-            </div>
-            <div>
-              <p> Members 1 Name</p>
-            </div>
-            <div>
-              <p> Members 1 Description</p>
-            </div>
-            <div>
-              <p> Members 1 Facebook</p>
-              <p> Members 1 Instagramm</p>
-              <p> Members 1 Email</p>
-            </div>
+          <div className={styles.about_members}>
+              <img src="img/team3.jpg"/>
+              <p className={styles.about_position}>GARDENER</p>
+              <p className={styles.about_position_name}>WALTER HARRIS</p>
+              <p className={styles.about_position_text}>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatupteoccaecat cupidatat not perspiciatis unde omnise natus error sit voluptatem accus.</p>
+              <div className={styles.social}>
+                  <a href="#"><FaFacebook className={styles.social_icons} /></a>
+                  <a href="#"><FaInstagram className={styles.social_icons} /></a>
+                  <a href="#"><FaTwitter className={styles.social_icons} /></a>
+              </div>
           </div>
-
-          {/* <div>
-                            <img src="../public/img/beeboss.jpg" />
-                            <p> Members 2 </p>
-                        </div>
-                        <div >
-                            <img src="../public/img/beeboss.jpg" />
-                            <p> Members 3 </p>
-                        </div> */}
-        </div>
-      </div>
+      </Container>
     </>
   );
 }

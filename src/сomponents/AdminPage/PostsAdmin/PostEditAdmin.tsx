79f4cc54ab1fadca63ad0a28a73dc614fb25c149
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-08-19 21:43:19 +0200

    add files to Posts components

--- src/сomponents/AdminPage/PostsAdmin/PostEditAdmin.tsx
+++ src/сomponents/AdminPage/PostsAdmin/PostEditAdmin.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import { IPostDto } from "../../Posts/interfaces/IPostDTO";
 import { Editor } from "@tinymce/tinymce-react";
 import { toast } from "react-toastify";
@@ -25,6 +25,12 @@ export default function PostEditAdmin(props: PostEditAdminProps): JSX.Element {
   const [text, setText] = useState("");
   const initText = textOfPost;
   const [value, setValue] = useState<string>();
+  const [imageData, setImageData] = useState<string | null>(null);
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const linkToServer = "http://localhost:8080";
+  const width = 1000;
+  const height = 300;
+
 
   const collectNewPostData = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
@@ -36,12 +42,32 @@ export default function PostEditAdmin(props: PostEditAdminProps): JSX.Element {
     }));
   };
 
-  async function handlerSavePost() {
-    if (text.trim()) {
+  const handleSavePost = async () => {
+    // e.preventDefault();
+    if (text.trim() != "") {
+      let linkVar: string | undefined = undefined;
+
+      if (imageData && selectedFile) {
+        const formData = new FormData();
+        formData.append("file", selectedFile);
+  
+        try {
+          const response = await axios.post(
+            `${linkToServer}/files/upload?width=${width}&height=${height}`,
+            formData
+          );
+          linkVar = response.data.id.toString();
+          console.log("File uploaded:", linkVar);
+        } catch (error) {
+          console.error("Error uploading file:", error);
+        }
+      }
+
+
       try {
-        await axios.put(`http://localhost:8080/api/posts/update/${idPost}`, {
+        await axios.put(`${linkToServer}/api/posts/update/${idPost}`, {
           titlePost,
-          linkToImg,
+          linkToImg: linkVar || linkToImg,
           shortPostDescription,
           textOfPost: text,
           authorId,
@@ -63,10 +89,20 @@ export default function PostEditAdmin(props: PostEditAdminProps): JSX.Element {
         progress: undefined,
         theme: "light",
       });
+      setValue("");
+      window.location.reload();
     }
-    setValue("");
-    window.location.reload();
   }
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files && event.target.files[0];
+    setSelectedFile(file);
+
+    if (file) {
+      const url = URL.createObjectURL(file);
+      setImageData(url);
+    }
+  };
 
   return (
     <>
@@ -86,15 +122,23 @@ export default function PostEditAdmin(props: PostEditAdminProps): JSX.Element {
 
         <div className="d-flex align-items-center fs-4 m-2">
           <label htmlFor="linkToImg" className="col-md-2 me-2 text-end">
-            Link to Img:
+            Link to Img: {linkToImg}
           </label>
-          <input
+          <div>
+              <input type="file" onChange={handleFileChange} />
+              <br />
+              {imageData && <img src={imageData} alt="Image" />}
+            </div>
+
+
+
+          {/* <input
             className="form-control fs-5"
             name="linkToImg"
             defaultValue={linkToImg}
             onChange={collectNewPostData}
             required
-          />
+          /> */}
         </div>
 
         <div className="d-flex align-items-center fs-4 m-2">
@@ -126,23 +170,22 @@ export default function PostEditAdmin(props: PostEditAdminProps): JSX.Element {
           value={value}
           init={{
             plugins:
-              "a11ychecker advcode advlist advtable anchor autocorrect autolink autoresize autosave casechange charmap checklist code codesample directionality editimage emoticons export footnotes formatpainter fullscreen help image importcss inlinecss insertdatetime link linkchecker lists media mediaembed mentions mergetags nonbreaking pagebreak pageembed permanentpen powerpaste preview quickbars save searchreplace table tableofcontents template tinydrive tinymcespellchecker typography visualblocks visualchars wordcount",
-            toolbar1:
-              "undo redo| removeformat fontfamily fontsize blocks bold italic strikethrough underline subscript superscript | alignleft aligncenter alignright alignjustify alignnone lineheight indent outdent | fullscreen help",
-            toolbar2:
-              "preview selectall copy cut paste pastetext searchreplace spellcheckdialog spellchecker | insertdatetime charmap checklist bullist numlist casechange | pagebreak | ltr rtl | visualblocks visualchars | hr wordcount",
-            toolbar3: "",
-            toolbar4:
-              "table tableinsertdialog advtablerownumbering tablecellprops tablecopyrow tablecutrow tabledelete tabledeletecol tabledeleterow tableinsertcolafter tableinsertcolbefore tableinsertrowafter tableinsertrowbefore tablemergecells tablepasterowafter tablepasterowbefore tableprops tablerowprops tablesplitcells tableclass tablecellclass tablecellvalign tablecellborderwidth tablecellborderstyle tablecaption tablecellbackgroundcolor tablecellbordercolor tablerowheader tablecolheader tableofcontents tableofcontentsupdate",
-            toolbar5:
-              "export emoticons image editimage fliph flipv rotateleft rotateright | link openlink unlink | media | backcolor forecolor",
-          }}
+                "advlist anchor autolink autoresize autosave charmap code codesample directionality emoticons fullscreen help image importcss  insertdatetime link linkchecker lists media nonbreaking pagebreak preview quickbars save searchreplace table  template tinydrive visualblocks visualchars wordcount",
+              toolbar1:
+                "undo redo| removeformat fontfamily fontsize blocks bold italic strikethrough underline subscript superscript | alignleft aligncenter alignright alignjustify alignnone lineheight indent outdent | fullscreen help",
+              toolbar2:
+                "preview selectall copy cut paste pastetext searchreplace spellcheckdialog spellchecker | insertdatetime charmap checklist bullist numlist casechange | pagebreak | ltr rtl | visualblocks visualchars | hr wordcount",
+              toolbar3:
+                "table tableinsertdialog advtablerownumbering tablecellprops tablecopyrow tablecutrow tabledelete tabledeletecol tabledeleterow tableinsertcolafter tableinsertcolbefore tableinsertrowafter tableinsertrowbefore tablemergecells tablepasterowafter tablepasterowbefore tableprops tablerowprops tablesplitcells tableclass tablecellclass tablecellvalign tablecellborderwidth tablecellborderstyle tablecaption tablecellbackgroundcolor tablecellbordercolor tablerowheader tablecolheader tableofcontents tableofcontentsupdate",
+              toolbar4:
+                "export emoticons image editimage fliph flipv rotateleft rotateright | link openlink unlink | media | backcolor forecolor",
+            }}
         />
 
         <button
           type="button"
           className="btn btn-primary m-2"
-          onClick={handlerSavePost}
+          onClick={handleSavePost}
         >
           Save post to Data Base
         </button>
--- src/сomponents/AdminPage/PostsAdmin/PostsCreationAdmin.tsx
+++ src/сomponents/AdminPage/PostsAdmin/PostsCreationAdmin.tsx
@@ -40,49 +40,55 @@ export default function PostsCreationAdmin(): JSX.Element {
       setIsValid(false);
       return;
     }
-  
+
     let linkVar: string | undefined = undefined;
-  
+
     if (imageData && selectedFile) {
       const formData = new FormData();
       formData.append("file", selectedFile);
-  
+
       try {
-        const response = await axios.post(`${linkToServer}/files/upload?width=${width}&height=${height}`, formData);
+        const response = await axios.post(
+          `${linkToServer}/files/upload?width=${width}&height=${height}`,
+          formData
+        );
         linkVar = response.data.id.toString();
         console.log("File uploaded:", linkVar);
       } catch (error) {
         console.error("Error uploading file:", error);
       }
     }
-  
-    if (linkVar){
-    try {
-      await axios.post(`${linkToServer}/api/posts`, {
-        titlePost,
-        linkToImg: linkVar,
-        shortPostDescription,
-        textOfPost: text,
-        authorId: "1",
-      });
-  
-      toast.success("Your post has been successfully sent!", {
-        position: "bottom-right",
-        autoClose: 3000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: false,
-        progress: undefined,
-        theme: "light",
-      });
-  
-      setValue("");
-      window.location.reload();
-    } catch (error) {
-      console.error("There was an error when sending a posts data to Back:", error);
+
+    if (linkVar) {
+      try {
+        await axios.post(`${linkToServer}/api/posts`, {
+          titlePost,
+          linkToImg: linkVar,
+          shortPostDescription,
+          textOfPost: text,
+          authorId: "1",
+        });
+
+        toast.success("Your post has been successfully sent!", {
+          position: "bottom-right",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: false,
+          progress: undefined,
+          theme: "light",
+        });
+
+        setValue("");
+        window.location.reload();
+      } catch (error) {
+        console.error(
+          "There was an error when sending a posts data to Back:",
+          error
+        );
+      }
     }
-  }
   };
 
   return (
@@ -107,7 +113,7 @@ export default function PostsCreationAdmin(): JSX.Element {
               Link to Img:
             </label>
             <div>
-              <input type="file" onChange={handleFileChange} />
+              <input type="file" onChange={handleFileChange} required/>
               <br />
               {imageData && <img src={imageData} alt="Image" />}
             </div>
@@ -141,16 +147,16 @@ export default function PostsCreationAdmin(): JSX.Element {
             initialValue={initText}
             value={value}
             init={{
+              // "a11ychecker advcode advtable autocorrect casechange checklist editimage export footnotes formatpainter inlinecss mediaembed mentions mergetags pageembed permanentpen powerpaste tableofcontents tinymcespellchecker typography"
               plugins:
-                "a11ychecker advcode advlist advtable anchor autocorrect autolink autoresize autosave casechange charmap checklist code codesample directionality editimage emoticons export footnotes formatpainter fullscreen help image importcss inlinecss insertdatetime link linkchecker lists media mediaembed mentions mergetags nonbreaking pagebreak pageembed permanentpen powerpaste preview quickbars save searchreplace table tableofcontents template tinydrive tinymcespellchecker typography visualblocks visualchars wordcount",
+                "advlist anchor autolink autoresize autosave charmap code codesample directionality emoticons fullscreen help image importcss  insertdatetime link linkchecker lists media nonbreaking pagebreak preview quickbars save searchreplace table  template tinydrive visualblocks visualchars wordcount",
               toolbar1:
                 "undo redo| removeformat fontfamily fontsize blocks bold italic strikethrough underline subscript superscript | alignleft aligncenter alignright alignjustify alignnone lineheight indent outdent | fullscreen help",
               toolbar2:
                 "preview selectall copy cut paste pastetext searchreplace spellcheckdialog spellchecker | insertdatetime charmap checklist bullist numlist casechange | pagebreak | ltr rtl | visualblocks visualchars | hr wordcount",
-              toolbar3: "",
-              toolbar4:
+              toolbar3:
                 "table tableinsertdialog advtablerownumbering tablecellprops tablecopyrow tablecutrow tabledelete tabledeletecol tabledeleterow tableinsertcolafter tableinsertcolbefore tableinsertrowafter tableinsertrowbefore tablemergecells tablepasterowafter tablepasterowbefore tableprops tablerowprops tablesplitcells tableclass tablecellclass tablecellvalign tablecellborderwidth tablecellborderstyle tablecaption tablecellbackgroundcolor tablecellbordercolor tablerowheader tablecolheader tableofcontents tableofcontentsupdate",
-              toolbar5:
+              toolbar4:
                 "export emoticons image editimage fliph flipv rotateleft rotateright | link openlink unlink | media | backcolor forecolor",
             }}
           />

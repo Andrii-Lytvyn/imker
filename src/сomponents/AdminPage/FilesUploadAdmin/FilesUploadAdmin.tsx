32ea9802d2278377085aa7d5833e26df48605608
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-08-22 22:43:32 +0200

    add DeleteFile

--- src/сomponents/AdminPage/FilesUploadAdmin/FilesUploadAdmin.tsx
+++ src/сomponents/AdminPage/FilesUploadAdmin/FilesUploadAdmin.tsx
@@ -1,4 +1,5 @@
 import { ChangeEvent, useEffect, useState } from "react";
+import { Button, Modal } from "react-bootstrap";
 import { IFilesListDto, initIFilesListDto } from "./interfaces/IFilesListDto";
 import { Pagination } from "@mui/material";
 import axios from "axios";
@@ -7,6 +8,8 @@ export default function FilesUploadAdmin(): JSX.Element {
   const [{ files, count, pages }, setFilesList] =
     useState<IFilesListDto>(initIFilesListDto);
   const [currentPage, setCurrentPage] = useState(1);
+  const [showConfirmModal, setShowConfirmModal] = useState(false);
+  const [selectedFileId, setSelectedFileId] = useState<number | null>(null);
   const itemsOnPage = 5;
   const linkToServer = "http://localhost:8080";
 
@@ -37,19 +40,48 @@ export default function FilesUploadAdmin(): JSX.Element {
     }
   };
 
+  const handleDelete = async () => {
+    if (selectedFileId !== null) {
+      try {
+        await axios.delete(`${linkToServer}/files/delete/${selectedFileId}`);
+        setShowConfirmModal(false);
+        setSelectedFileId(null);
+
+        const response = await axios.get(
+          `${linkToServer}/files?page=${currentPage - 1}&items=${itemsOnPage}`
+        );
+        setFilesList(await response.data);
+      } catch (error) {
+        console.error("Error during file deletion:", error);
+      }
+    }
+  };
+
   return (
-    <div className="container" >
-      <p>Total files: {count}</p>
-      <Pagination
-        count={pages}
-        page={currentPage}
-        size="large"
-        onChange={getAnotherPage}
-      />
+    <div className="container col-md-12 mt-3 mb-5">
+      <p className="fs-4">Total files: {count}</p>
+      <div className="col-md-12 d-flex justify-content-center mt-3 mb-4">
+        <Pagination
+          count={pages}
+          page={currentPage}
+          size="large"
+          onChange={getAnotherPage}
+        />
+      </div>
       {files.map(
         ({ id, creationTime, originalName, storedName, fileType, size }) => (
           <div key={id}>
             <hr />
+            <button
+              className="btn btn-danger"
+              onClick={() => {
+                setSelectedFileId(+id);
+                setShowConfirmModal(true);
+              }}
+            >
+              Delete this image from Data Base
+            </button>
+
             <div className="col-md-12 d-flex align-items-center mt-3 mb-4">
               <div className="col-md-5 m-2">
                 <img
@@ -64,25 +96,63 @@ export default function FilesUploadAdmin(): JSX.Element {
                   }}
                 />
               </div>
-              <div className="col-md-8 fs-4 ms-4">
-                <p>Image id: {id}</p>
-                <p>Created: {creationTime}</p>
-                <p>Original name: {originalName}</p>
-                <p>Stored name: {storedName}</p>
-                <p>File type: {fileType}</p>
-                <p>File size: {size} bytes</p>
+              <div className="col-md-8 ms-4">
+                <p>
+                  <span style={{ fontWeight: "bold" }}>Image id:</span> {id}
+                </p>
+                <p style={{ marginTop: "5px" }}>
+                  <span style={{ fontWeight: "bold" }}>Created:</span> {creationTime}
+                </p>
+                <p style={{ marginTop: "5px" }}>
+                  <span style={{ fontWeight: "bold" }}>Original name:</span> {originalName}
+                </p>
+                <p style={{ marginTop: "5px" }}>
+                  <span style={{ fontWeight: "bold" }}>Stored name:</span> {storedName}
+                </p>
+                <p style={{ marginTop: "5px" }}>
+                  <span style={{ fontWeight: "bold" }}>File type:</span> {fileType}
+                </p>
+                <p style={{ marginTop: "5px" }}>
+                  <span style={{ fontWeight: "bold" }}>File size:</span> {size} bytes
+                </p>
               </div>
             </div>
           </div>
         )
       )}
       <hr />
-      <Pagination
-        count={pages}
-        page={currentPage}
-        size="large"
-        onChange={getAnotherPage}
-      />
+      <div className="col-md-12 d-flex justify-content-center mt-3 mb-4">
+        <Pagination
+          count={pages}
+          page={currentPage}
+          size="large"
+          onChange={getAnotherPage}
+        />
+      </div>
+
+      <Modal show={showConfirmModal} onHide={() => setShowConfirmModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Confirm Deletion</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Are you sure you want to delete this file? <br />
+          <br /> NOTICE: Upon deleting an image from the database, it will
+          become unavailable when attempting to display it on the website,
+          leading to errors. Make sure this image is no longer in use before
+          proceeding with its deletion.
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="secondary"
+            onClick={() => setShowConfirmModal(false)}
+          >
+            Cancel
+          </Button>
+          <Button variant="danger" onClick={handleDelete}>
+            Delete
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }

75de47fec14c51954e149eb130f4b7c052fb94da
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-08-25 00:42:57 +0200

    add PostsPanel

---
+++ src/сomponents/Posts/PostsPanel/PostsPanel.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  IPostsDto,
+  initIPostsDto,
+} from "../../AdminPage/PostsAdmin/interfaces/IPostsDto";
+import { Link } from "react-router-dom";
+
+export default function PostsPanel() {
+  const [posts, setPosts] = useState<IPostsDto>(initIPostsDto);
+  const itemsOnPage = 3;
+  const linkToServer = "http://localhost:8080";
+
+  useEffect(() => {
+    async function getListOfPosts() {
+      try {
+        const response = await axios.get(
+          `${linkToServer}/api/posts?page=0&items=${itemsOnPage}&orderBy=creationTimePost&desk=true`
+        );
+        setPosts(response.data);
+      } catch (error) {
+        console.error("Error during request execution:", error);
+      }
+    }
+    getListOfPosts();
+  }, []);
+
+  return (
+    <>
+      {posts && (
+        <div className="container">
+          {posts.posts.map(
+            ({
+              idPost,
+              creationTimePost,
+              titlePost,
+              linkToImg,
+              shortPostDescription,
+              // textOfPost,
+              authorName,
+            }) => (
+              <div key={idPost}>
+                <hr />
+                <p>Post id: {idPost}</p>
+                <p>Created: {creationTimePost}</p>
+                {authorName && <p>Author name: {authorName}</p>}
+                <Link to={`/posts/${idPost}`} className="fs-4">
+                  {titlePost}
+                </Link>
+                <br />
+                <img
+                  src={linkToServer + "/api/files/" + linkToImg}
+                  alt={"post img" + idPost}
+                  onError={(e) => {
+                    const target = e.target as HTMLImageElement;
+                    target.src = "/img/imgNotFound.png";
+                  }}
+                />
+                <div>{shortPostDescription}</div>
+              </div>
+            )
+          )}
+        </div>
+      )}
+    </>
+  );
+}

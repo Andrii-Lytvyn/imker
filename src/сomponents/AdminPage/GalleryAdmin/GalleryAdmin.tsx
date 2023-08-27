2f28d426a948e6b2c070ce06134d430edbbb1862
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-08-28 01:54:33 +0200

    add files upload to gallery

--- src/сomponents/AdminPage/GalleryAdmin/GalleryAdmin.tsx
+++ src/сomponents/AdminPage/GalleryAdmin/GalleryAdmin.tsx
@@ -1,12 +1,14 @@
 import { ChangeEvent, useEffect, useState } from "react";
 import { Pagination } from "@mui/material";
 import axios from "axios";
-import linkToServer from "../../globalLinkToServer";
 import { IGalleryPhotos, initIFilesListDto } from "./interfaces/IGalleryPhotos";
+import FilesUpload from "./FilesUpload";
+import linkToServer from "../../globalLinkToServer";
 
 export default function GalleryAdmin(): JSX.Element {
   const [{ photos, count, pages }, setFilesList] =
     useState<IGalleryPhotos>(initIFilesListDto);
+  const [reloading, setReloading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const itemsOnPage = 5;
 
@@ -25,12 +27,19 @@ export default function GalleryAdmin(): JSX.Element {
       }
     }
     getListOfFiles();
-  }, []);
+    setReloading(false);
+  }, [reloading]);
+
+  const handleReload = () => {
+    setReloading(!reloading); 
+  };
 
   const getAnotherPage = async (_: ChangeEvent<unknown>, value: number) => {
     try {
       const response = await axios.get(
-        `${linkToServer}/api/gallery?page=${value - 1}&items=${itemsOnPage}&orderBy=creationTimePhoto&desk=true`
+        `${linkToServer}/api/gallery?page=${
+          value - 1
+        }&items=${itemsOnPage}&orderBy=creationTimePhoto&desk=true`
       );
       setFilesList(await response.data);
       setCurrentPage(value);
@@ -57,54 +66,62 @@ export default function GalleryAdmin(): JSX.Element {
   };
 
   return (
-    <div className="container col-md-12 mt-3 mb-5">
-      <p className="fs-4">Total files: {count}</p>
-      <div className="col-md-12 d-flex justify-content-center mt-3 mb-4">
-        <Pagination
-          count={pages}
-          page={currentPage}
-          size="large"
-          onChange={getAnotherPage}
-        />
-      </div>
-      {photos.map(({ id, linkToImg }) => (
-        <div key={id}>
-          <hr />
-          <button
-            className="btn btn-danger"
-            onClick={() => {
-              handleDelete(+id);
-            }}
-          >
-            Delete this image from Data Base
-          </button>
+    <>
+      <div className="container col-md-12 bg-light border rounded mt-4 p-4">
+        <p className="fs-3 mb-4">Add new photo to gallery</p>
 
-          <div className="col-md-12 d-flex align-items-center mt-3 mb-4">
-            <div className="col-md-5 m-2">
-              <img
-                src={linkToServer + "/api/files/" + linkToImg}
-                alt="image"
-                style={{
-                  maxWidth: "100%",
-                  maxHeight: "100%",
-                  width: "600px",
-                  height: "300px",
-                  objectFit: "contain",
-                }}
-              />
+        <FilesUpload handleReload = {() => handleReload()} />
+      </div>
+
+      <div className="container col-md-12 mt-3 mb-5">
+        <p className="fs-4">Total files in gallery: {count}</p>
+        <div className="col-md-12 d-flex justify-content-center mt-3 mb-4">
+          <Pagination
+            count={pages}
+            page={currentPage}
+            size="large"
+            onChange={getAnotherPage}
+          />
+        </div>
+        {photos.map(({ id, linkToImg }) => (
+          <div key={id}>
+            <hr />
+            <button
+              className="btn btn-danger"
+              onClick={() => {
+                handleDelete(+id);
+              }}
+            >
+              Delete this image from Data Base
+            </button>
+
+            <div className="col-md-12 d-flex align-items-center mt-3 mb-4">
+              <div className="col-md-5 m-2">
+                <img
+                  src={linkToServer + "/api/files/" + linkToImg}
+                  alt="image"
+                  style={{
+                    maxWidth: "100%",
+                    maxHeight: "100%",
+                    width: "600px",
+                    height: "300px",
+                    objectFit: "contain",
+                  }}
+                />
+              </div>
             </div>
           </div>
+        ))}
+        <hr />
+        <div className="col-md-12 d-flex justify-content-center mt-3 mb-4">
+          <Pagination
+            count={pages}
+            page={currentPage}
+            size="large"
+            onChange={getAnotherPage}
+          />
         </div>
-      ))}
-      <hr />
-      <div className="col-md-12 d-flex justify-content-center mt-3 mb-4">
-        <Pagination
-          count={pages}
-          page={currentPage}
-          size="large"
-          onChange={getAnotherPage}
-        />
       </div>
-    </div>
+    </>
   );
 }

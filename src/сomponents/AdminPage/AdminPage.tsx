993e774464b7fea8de60477824f20171afa82df5
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-08-28 20:58:41 +0200

    add AdminPage content

--- src/сomponents/AdminPage/AdminPage.tsx
+++ src/сomponents/AdminPage/AdminPage.tsx
@@ -56,65 +56,49 @@ export default function AdminPage() {
   };
 
   return (
-    <Container>
-      <Box sx={{ width: "100%" }}>
+    <>
+      <Container>
         <Box
           sx={{
             maxWidth: { xs: 320, sm: "100%" },
             bgcolor: "background.paper",
           }}
+          className="d-flex justify-content-center mt-4 p-2 fs-2"
         >
           <Tabs
-            className="m-2 p-3"
             value={value}
             onChange={handleChange}
             variant="scrollable"
             scrollButtons="auto"
             aria-label="scrollable auto tabs example"
           >
-            <Tab label="about us" />
-            <Tab label="addmember" />
-            <Tab label="Team Edit Member" />
-            <Tab label="contact us" />
-            <Tab label="Posts" />
-            <Tab label="Files Upload" />
-            <Tab label="Gallery" />
-            <Tab label="Add Event" />
-            <Tab label="Edit Event" />
-            <Tab label="Edit All Events" />
+            <Tab className="fs-5" label="BLOG" />
+            <Tab className="fs-5" label="VERANSTALTUNGEN" />
+            <Tab className="fs-5" label="Kontaktieren Sie uns" />
+            <Tab className="fs-5" label="Mitglieder der Gemeinschaft" />
+            <Tab className="fs-5" label="Gallery" />
+            <Tab className="fs-5" label="Files Upload" />
           </Tabs>
         </Box>
-        <CustomTabPanel value={value} index={0}>
-          <TeamAdmin />
-        </CustomTabPanel>
-        <CustomTabPanel value={value} index={1}>
-          <TeamAddMemberAdmin />
-        </CustomTabPanel>
-        <CustomTabPanel value={value} index={2}>
-          <TeamEditMemberAdmin />
-        </CustomTabPanel>
-        <CustomTabPanel value={value} index={3}>
-          <ContactUsAdmin />
-        </CustomTabPanel>
-        <CustomTabPanel value={value} index={4}>
-          <PostsAdmin />
-        </CustomTabPanel>
-        <CustomTabPanel value={value} index={5}>
-          <FilesUploadAdmin />
-        </CustomTabPanel>
-        <CustomTabPanel value={value} index={6}>
-          <GalleryAdmin />
-        </CustomTabPanel>
-        <CustomTabPanel value={value} index={7}>
-          <AddEventAdmin />
-        </CustomTabPanel>
-        <CustomTabPanel value={value} index={8}>
-          <EditEventAdmin />
-        </CustomTabPanel>
-        <CustomTabPanel value={value} index={9}>
-          <EditAllEvents />
-        </CustomTabPanel>
-      </Box>
-    </Container>
+      </Container>
+      <CustomTabPanel value={value} index={0}>
+        <PostsAdmin />
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={1}>
+        <EditAllEvents />
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={2}>
+        <ContactUsAdmin />
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={3}>
+        <TeamAdmin />
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={4}>
+        <GalleryAdmin />
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={5}>
+        <FilesUploadAdmin />
+      </CustomTabPanel>
+    </>
   );
 }

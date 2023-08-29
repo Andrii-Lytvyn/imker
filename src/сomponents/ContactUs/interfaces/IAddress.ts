04c9a2955f085bf47dd4e76e87eabe4b8068401a
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-08-29 23:24:29 +0200

    add GET addres to ContactUs

---
+++ src/сomponents/ContactUs/interfaces/IAddress.ts
@@ -0,0 +1,13 @@
+export interface IAddress {
+  id: number;
+  address: string;
+  phone: string;
+  email: string;
+
+}
+export const initIUserAccountInfo = {
+  id: -1,
+  address: "",
+  phone: "",
+  email: "",
+}

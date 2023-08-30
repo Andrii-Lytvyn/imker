9182415424fd0c105a4eb960967c3f3d5b5fe909
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-08-30 02:52:01 +0200

    refactor Gallery to GET files from server

--- src/сomponents/Gallery/Gallery.tsx
+++ src/сomponents/Gallery/Gallery.tsx
@@ -1,96 +1,115 @@
 import { ChangeEvent, useEffect, useState } from "react";
 import styles from "./Gallery.module.css";
 import axios from "axios";
-import { imageObjRequest } from "./imgObj";
-import { useSearchParams } from "react-router-dom";
 import { Pagination } from "@mui/material";
-import { IImgData } from "./interfaces/IImgData";
 import Modal from "./Modal/Modal";
-import {Container} from "react-bootstrap";
-
-const baseURL = "https://63bb362a32d17a50908a3770.mockapi.io";
-
-// получение картинок
-const imgDataFromMockApi = async () => {
-  try {
-    const { data } = await axios.get(`${baseURL}/user_login`);
-
-    return data;
-  } catch (error) {
-    console.log("🚀  error:", error);
-  }
-};
+import { Container } from "react-bootstrap";
+import linkToServer from "../globalLinkToServer";
+import {
+  IGalleryPhotos,
+  initIFilesListDto,
+} from "../AdminPage/GalleryAdmin/interfaces/IGalleryPhotos";
 
 const Gallery = (): JSX.Element => {
-  const imagesPerPage = 16;
-  const [data, setDataImages] = useState<IImgData[]>([]);
-  const [modalHide, setModalHide] = useState(false);
+  const [isModalShow, setIsModalShow] = useState(false);
   const [modalImage, setModalImage] = useState<string | undefined>("");
-  const [searchParams, setSearchParams] = useSearchParams();
-  const pageNumber = searchParams.get("page") ?? "1";
+
+  const [{ photos, pages }, setFilesList] =
+    useState<IGalleryPhotos>(initIFilesListDto);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsOnPage = 12;
 
   useEffect(() => {
-    const fetchImages = async () => {
-      // setDataImages(await imgDataFromMockApi()); //получаем каринки запросом
-       setDataImages(imageObjRequest.data); //получаем картинки из файла
-    };
-    fetchImages();
+    async function getListOfFiles() {
+      try {
+        const response = await axios.get(
+          `${linkToServer}/api/gallery?page=0&items=${itemsOnPage}&orderBy=creationTimePhoto&desk=true`
+        );
+        setFilesList(response.data);
+        setCurrentPage(1);
+      } catch (error) {
+        console.error("Error during request execution:", error);
+      }
+    }
+    getListOfFiles();
   }, []);
 
-  const getLinkParams = (_: ChangeEvent<unknown>, value: number) => {
-    setSearchParams({ page: value.toString() });
+  const getAnotherPage = async (_: ChangeEvent<unknown>, value: number) => {
+    try {
+      const response = await axios.get(
+        `${linkToServer}/api/gallery?page=${
+          value - 1
+        }&items=${itemsOnPage}&orderBy=creationTimePhoto&desk=true`
+      );
+      setFilesList(await response.data);
+      setCurrentPage(value);
+    } catch (error) {
+      console.error("Error during request execution:", error);
+    }
   };
 
-  const modalShow = (id: string) => {
-    setModalHide(true);
-    const imgModalShow = data.find((item) => item.id === id);
-    setModalImage(imgModalShow?.img);
+  const modalShow = (linkToImg: number) => {
+    setIsModalShow(true);
+    setModalImage(""+linkToImg);
   };
 
   return (
     <div>
       <h2>Our gallery</h2>
       <Container>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio perspiciatis laboriosam animi, ipsum, dolorum ab laudantium accusamus commodi ducimus nisi esse repellendus ratione molestias sit magni soluta perferendis vero pariatur. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio perspiciatis laboriosam animi, ipsum, dolorum ab laudantium accusamus commodi ducimus nisi esse repellendus ratione molestias sit magni soluta perferendis vero pariatur. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio perspiciatis laboriosam animi, ipsum, dolorum ab laudantium accusamus commodi ducimus nisi esse repellendus ratione molestias sit magni soluta perferendis vero pariatur. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio perspiciatis laboriosam animi, ipsum, dolorum ab laudantium accusamus commodi ducimus nisi esse repellendus ratione molestias sit magni soluta perferendis vero pariatur. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio perspiciatis laboriosam animi, ipsum, dolorum ab laudantium accusamus commodi ducimus nisi esse repellendus ratione molestias
-
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio
+          perspiciatis laboriosam animi, ipsum, dolorum ab laudantium accusamus
+          commodi ducimus nisi esse repellendus ratione molestias sit magni
+          soluta perferendis vero pariatur. Lorem ipsum dolor sit, amet
+          consectetur adipisicing elit. Distinctio perspiciatis laboriosam
+          animi, ipsum, dolorum ab laudantium accusamus commodi ducimus nisi
+          esse repellendus ratione molestias sit magni soluta perferendis vero
+          pariatur. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Distinctio perspiciatis laboriosam animi, ipsum, dolorum ab laudantium
+          accusamus commodi ducimus nisi esse repellendus ratione molestias sit
+          magni soluta perferendis vero pariatur. Lorem ipsum dolor sit, amet
+          consectetur adipisicing elit. Distinctio perspiciatis laboriosam
+          animi, ipsum, dolorum ab laudantium accusamus commodi ducimus nisi
+          esse repellendus ratione molestias sit magni soluta perferendis vero
+          pariatur. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Distinctio perspiciatis laboriosam animi, ipsum, dolorum ab laudantium
+          accusamus commodi ducimus nisi esse repellendus ratione molestias
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        </p>
       </Container>
       <Container>
-      <div className={styles.gallery_container}>
-        <ul className={styles.grid}>
-          {data
-            .map((item) => (
+        <div className={styles.gallery_container}>
+          <ul className={styles.grid}>
+            {photos.map((item) => (
               <li key={item.id} className={styles.card}>
                 <img
                   id={item.id}
-                  src={item.img}
+                  src={linkToServer + "/api/files/" + item.linkToImg}
                   alt={item.id}
                   className={styles.card_item}
-                  onClick={() => modalShow(item.id)}
+                  onClick={() => modalShow(item.linkToImg)}
                 />
               </li>
-            ))
-            .slice(
-              (+pageNumber - 1) * imagesPerPage,
-              (+pageNumber - 1) * imagesPerPage + imagesPerPage
-            )}
-        </ul>
+            ))}
+          </ul>
 
-        <div className={styles.pagination_container}>
-          <Pagination
-            count={Math.ceil(+data.length / imagesPerPage)}
-            page={+pageNumber}
-            size="large"
-            onChange={getLinkParams}
-          />
+          <div className={styles.pagination_container}>
+            <Pagination
+              count={pages}
+              page={currentPage}
+              size="large"
+              onChange={getAnotherPage}
+            />
+          </div>
         </div>
-      </div>
-      {modalHide ? (
-        <Modal setModalHide={setModalHide} modalImage={modalImage} />
-      ) : (
-        ""
-      )}
-        </Container>
+        {isModalShow && 
+          <Modal
+            setModalHide={setIsModalShow}
+            modalImage={linkToServer + "/api/files/" + modalImage}
+          />
+        }
+      </Container>
     </div>
   );
 };

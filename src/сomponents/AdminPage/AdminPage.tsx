a03ec8ca7a2086e6f75d003d11f0093e7919c3c0
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-09-05 03:02:06 +0200

    fix only admin access to AdminPage

--- src/сomponents/AdminPage/AdminPage.tsx
+++ src/сomponents/AdminPage/AdminPage.tsx
@@ -12,6 +12,9 @@ import { Container } from "react-bootstrap";
 import styles from "../AdminPage/PostsAdmin/PostAdmin.module.css";
 import AddEventAdmin from "./EventsAdmin/AddEventAdmin";
 import UsersAdmin from "./UserAdmin/UsersAdmin";
+import { IUserDto, initIUserDto } from "./UserAdmin/interfaces/IUserDto";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 interface TabPanelProps {
   children?: React.ReactNode;
@@ -41,6 +44,26 @@ function CustomTabPanel(props: TabPanelProps) {
 
 export default function AdminPage() {
   const [value, setValue] = React.useState(0);
+  const [me, setMe] = useState<IUserDto>(initIUserDto);
+  const isLogined = localStorage.getItem("IMKER");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (isLogined === "true") {
+          const getMyId = await axios.get(`/api/me`, {
+            withCredentials: true,
+          });
+          const userDto = getMyId.data;
+          setMe(userDto);
+        }
+      } catch (error) {
+        console.error("Error during request execution:", error);
+      }
+    };
+
+    fetchData();
+  }, [isLogined, me.role]);
 
   const handleChange = (_event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
@@ -48,57 +71,61 @@ export default function AdminPage() {
 
   return (
     <>
-      <div className={styles.bgndPost}></div>
-      <Container>
-        <Box
-          sx={{
-            maxWidth: { xs: 320, sm: "100%" },
-            bgcolor: "background.paper",
-          }}
-          className="d-flex justify-content-center mt-4 p-2 fs-2"
-        >
-          <Tabs
-            value={value}
-            onChange={handleChange}
-            variant="scrollable"
-            scrollButtons="auto"
-            aria-label="scrollable auto tabs example"
-          >
-            <Tab className="fs-5" label="BLOG" />
-            <Tab className="fs-5" label="VERANSTALTUNGEN" />
-            <Tab className="fs-5" label="Kontaktieren Sie uns" />
-            <Tab className="fs-5" label="Mitglieder der Gemeinschaft" />
-            <Tab className="fs-5" label="Gallery" />
-            <Tab className="fs-5" label="Users" />
-            <Tab className="fs-5" label="Files Upload" />
-          </Tabs>
-        </Box>
-      </Container>
-      <CustomTabPanel value={value} index={0}>
-        <PostsAdmin />
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={1}>
-        <div className="container">
-          <AddEventAdmin />
+      {me.role === "ADMIN" && (
+        <div>
+          <div className={styles.bgndPost}></div>
+          <Container>
+            <Box
+              sx={{
+                maxWidth: { xs: 320, sm: "100%" },
+                bgcolor: "background.paper",
+              }}
+              className="d-flex justify-content-center mt-4 p-2 fs-2"
+            >
+              <Tabs
+                value={value}
+                onChange={handleChange}
+                variant="scrollable"
+                scrollButtons="auto"
+                aria-label="scrollable auto tabs example"
+              >
+                <Tab className="fs-5" label="BLOG" />
+                <Tab className="fs-5" label="VERANSTALTUNGEN" />
+                <Tab className="fs-5" label="Kontaktieren Sie uns" />
+                <Tab className="fs-5" label="Mitglieder der Gemeinschaft" />
+                <Tab className="fs-5" label="Gallery" />
+                <Tab className="fs-5" label="Users" />
+                <Tab className="fs-5" label="Files Upload" />
+              </Tabs>
+            </Box>
+          </Container>
+          <CustomTabPanel value={value} index={0}>
+            <PostsAdmin />
+          </CustomTabPanel>
+          <CustomTabPanel value={value} index={1}>
+            <div className="container">
+              <AddEventAdmin />
+            </div>
+          </CustomTabPanel>
+          <CustomTabPanel value={value} index={2}>
+            <ContactUsAdmin />
+          </CustomTabPanel>
+          <CustomTabPanel value={value} index={3}>
+            <div className="container">
+              <TeamAdmin />
+            </div>
+          </CustomTabPanel>
+          <CustomTabPanel value={value} index={4}>
+            <GalleryAdmin />
+          </CustomTabPanel>
+          <CustomTabPanel value={value} index={5}>
+            <UsersAdmin />
+          </CustomTabPanel>
+          <CustomTabPanel value={value} index={6}>
+            <FilesUploadAdmin />
+          </CustomTabPanel>
         </div>
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={2}>
-        <ContactUsAdmin />
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={3}>
-        <div className="container">
-          <TeamAdmin />
-        </div>
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={4}>
-        <GalleryAdmin />
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={5}>
-        <UsersAdmin />
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={6}>
-        <FilesUploadAdmin />
-      </CustomTabPanel>
+      )}
     </>
   );
 }
